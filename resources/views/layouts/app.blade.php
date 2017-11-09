46461d0e77df07f6b461991893c6b0003110f5b5
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-11-09 13:41:09 -0500

    added home link to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li><a href="{{ route('home') }}">Home</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
